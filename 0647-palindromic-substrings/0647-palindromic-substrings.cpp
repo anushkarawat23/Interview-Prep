8bd14a3d10f8fc2659fdcef936ac99726afed009
Anushka Rawat <112893429+anushkarawat23@users.noreply.github.com>
2024-08-30 02:49:36 +0530

    Time: 31 ms (35.23%), Space: 12.2 MB (47.99%) - LeetHub

--- 0647-palindromic-substrings/0647-palindromic-substrings.cpp
+++ 0647-palindromic-substrings/0647-palindromic-substrings.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-
+/*    TC = O(N^2)
     int palindrome(string s, int left, int right)
     {
         int ans = 0;
@@ -22,7 +22,40 @@ public:
             ans += palindrome(s,i,i+1);  //counting even palindromes
         }
 
-        return ans;
-        
+        return ans; 
     }
+    */
+
+    //RECURSION + MEMOIZATION
+    int t[1001][1001]; 
+    bool checkPalin(string &s, int i, int j)
+    {
+        if(i > j)
+            return true;
+
+        if(t[i][j] != -1)
+            return t[i][j];
+
+        if(s[i] == s[j])
+            return t[i][j] = checkPalin(s, i+1, j-1);
+
+        return t[i][j] = false;
+    }
+
+    int countSubstrings(string s)
+    {
+        int count = 0;
+        int n = s.size();
+        memset(t, -1, sizeof(t));
+        for(int i = 0; i < n ; i++)
+        {
+            for(int j = i ; j < n ; j++)
+            {
+                if(checkPalin(s, i, j))
+                    count++;
+            }
+        }
+        return count;
+    }
+
 };
