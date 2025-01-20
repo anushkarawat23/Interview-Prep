1238732e160b161de764aaafb47e27318bd10a97
Anushka Rawat <112893429+anushkarawat23@users.noreply.github.com>
2025-01-20 14:14:32 +0530

    Time: 4 ms (62.24%), Space: 14.8 MB (44.13%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -1,40 +1,19 @@
 class Solution {
 public:
-    vector<int> twoSum(vector<int>& nums, int target) 
-    {
-        /* BRUTE FORCE   O(N^2)
-        vector<int> ans;
-        for(int i=0;i<nums.size();i++)
-        {
-            for(int j=i+1;j<nums.size();j++)
-            {
-                if(nums[i]+nums[j]==target)
-                {
-                    ans.push_back(i);
-                    ans.push_back(j);
-                }
-            }
-        }
-        return ans;
-       */
-
-
-        //OPTIMIZED APPROACH
-
-        unordered_map<int,int> map;
-        vector<int> ans;
-        int i ;
-        for(i = 0 ; i < nums.size() ; i++)
-        {
-            if( map.find( (target - nums[i]) ) != map.end())
-            {
-                ans.push_back(i);
-                ans.push_back(map[ target - nums[i] ]);
-                return ans;
-            }
-            map[nums[i]] = i;
-        }
-        return ans;
+    vector<int> twoSum(vector<int>& nums, int target) {
         
+        unordered_map<int,int> hash;
+        
+        for(int i = 0; i < nums.size() ; i++)
+        {
+            if(hash.find(nums[i]) != hash.end())
+            {
+                return {hash[nums[i]], i};
+            }
+
+            hash[target - nums[i]] = i;
+        }
+
+        return {0,0};
     }
 };
